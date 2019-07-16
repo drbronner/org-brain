f5fdfefe1fcc015c0c6adce3514fcc235c568bab
Galen Menzel <galen.menzel@utexas.edu>
2019-07-16 10:29:51 -0700

    Fix org-brain-goto: show entries on own lines

--- org-brain.el
+++ org-brain.el
@@ -1112,6 +1112,7 @@ Unless GOTO-FILE-FUNC is nil, use `pop-to-buffer-same-window' for opening the en
     (widen)
     (goto-char (marker-position marker))
     (when (org-at-heading-p)
+      (org-show-entry)
       (org-show-subtree)))
   entry)
 
